7c49028d558be231953f7b9b2ff0ca771099c5ad
RodrigoBatista27 <102333705+RodrigoBatista27@users.noreply.github.com>
2023-04-04 14:40:28 -0300

    Criando Main.kt

---
+++ main.kt
@@ -0,0 +1,11 @@
+
+
+fun main() {
+
+    val user1 = User("Rodrigo", "rodrigo@gmail.com", "12345")
+    val user2 = User("Rafael", "rafael@gmail.com", "12345")
+
+    val formations = EducationalContentRepository
+
+    val academicFormationRepo = AcademicFormationRepo()
+}
